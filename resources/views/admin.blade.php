39ce637ae55f84c14d30959ad9579afdba84d653
Sara T <34686850+inComesCrane@users.noreply.github.com>
2018-01-14 16:30:42 +0100

    paneli admin dhe moderator

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="col-12 col-md-12">
+        <p class="float-right d-md-none">
+            <button type="button" class="btn btn-primary btn-sm" data-toggle="offcanvas">Toggle nav</button>
+        </p>
+        <div class="jumbotron">
+            <h1>Hello, sheep master</h1><br/>
+
+            <div>
+                Ketu mund te kontrolloni fatet e sheep-eve. Ata jane nen meshiren tuaj.
+            </div>
+            <table border="2px solid black" style="min-width:700px; margin: 10px; background-color: #fff;">
+                <th colspan="2" style="padding: 10px; background-color: #f2f2ef;"> Admin Privileges</th>
+                <tr >
+                    <td style="padding: 10px;" onclick="window.location='{{ url("/users") }}'"> View all users </td>
+                    <td style="padding: 10px;"> Add user </td>
+                </tr>
+            </table>
+        </div>
+
+
+    </div><!--/span-->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,23 @@ Route::delete('exams/{id}', 'ExamsController@destroy');
 Route::get('login', 'Auth\AuthController@redirectToProvider');
 Route::get('login/callback', 'Auth\AuthController@handleProviderCallback');
 Route::post('logout', 'Auth\AuthController@logout');
+
+Route::get('/admin', function () {
+    if (Auth::user() && Auth::user()->role == 3)
+        return view( 'admin');
+    return view('homes');
+});
+
+Route::get('/moderator', function () {
+    if (Auth::user() && Auth::user()->role == 2)
+        return view( 'moderator');
+    return view('homes');
+});
+
+Route::get('/users', function () {
+    if (Auth::user() && Auth::user()->role != 1){
+        $users = DB::table('users')->get();
+        return view( 'list', ['users' => $users]);
+    }
+});
+
